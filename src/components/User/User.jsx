ec80e79a88eef918e8d726230c31d96856197488
jaky-sheikh <jaky@eu4m.eu>
2025-01-12 21:36:16 +0600

    load single user data and dynamic route

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const User = ({ user }) => {
@@ -14,6 +15,7 @@ const User = ({ user }) => {
             <h2>{name}</h2>
             <p>Email: {email}</p>
             <p>{phone}</p>
+            <Link to={`/user/${id}`}>Show Details</Link>
         </div>
     );
 };
