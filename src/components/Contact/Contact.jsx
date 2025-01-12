6361bdc3ef926d112c277e8d56ba49680f42d68d
jaky-sheikh <jaky@eu4m.eu>
2025-01-12 16:47:50 +0600

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>Contact Us</h2>
+            <p>Need any query!Feel free to contact with us</p>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,12 @@
+
+
+const Users = () => {
+    return (
+        <div>
+            <h2>Our Users</h2>
+            <p>Our fantastic users</p>
+        </div>
+    );
+};
+
+export default Users;
