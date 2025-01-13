1f167fb1149c4c5e2839ed25a21439ed14a0f8ba
jaky-sheikh <jaky@eu4m.eu>
2025-01-13 08:27:14 +0600

    useNavigate and not found message function

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,18 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post = ({ post }) => {
     const { id, title } = post;
+
+    const navigate = useNavigate();
+
     const postStyle = {
         border: '2px solid tomato',
         padding: '5px',
         borderRadius: '20px',
         marginBottom: '10px'
     }
+
+    const handleShowDetail = () => {
+        navigate(`/post/${id}`);
+    }
+
     return (
         <div style={postStyle}>
             <h3>Post of Id: {id}</h3>
             <p>{title}</p>
             <Link to={`/post/${id}`}>Post Detail</Link>
+            <button onClick={handleShowDetail}>Click to see details</button>
         </div>
     );
 };
